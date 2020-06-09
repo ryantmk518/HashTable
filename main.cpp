9897d4cb3a9f8160a38e9a18c8408863e0fa0573
Ryan Thammakhoune <ryantmk518@gmail.com>
2020-06-09 06:34:59 +0100

    Random student works

--- main.cpp
+++ main.cpp
@@ -22,11 +22,10 @@ struct Student{
 
 
 Student** addStudent(Student** &array, Student* ptrStu, int &size);
-Student** reAdd(Student** &array, Student* student, int &size);
 void deleteStu(vector<Student*>* stuList);
 void print(Student** &array, int size);
 int hashFunction(Student* stu, int size);
-Student* randomStu();
+Student* randomStu(vector<char*> first, vector<char*> last);
 Student* endList(Student* &start);
 void printRow(Student* start);
 int checkCollision(Student* start, int count);
@@ -35,6 +34,40 @@ int checkRehash(Student** array, int size);
 
 
 int main() {
+
+
+  vector<char*> randFirst;
+  ifstream file;
+  file.open("fname.txt"); //Open the inputted file name
+  char* output = new char[999];
+  int i = 0;
+  while(!file.eof()) { //If has not reached end of file
+    file.get(output[i]);
+    i++;
+  }
+  char* split = strtok(output, " ");
+  while (split != NULL) {
+    randFirst.push_back(split);
+    split = strtok(NULL, " ");
+  }
+  cout << "First done" << endl;
+  
+  vector<char*> randLast;
+  ifstream file2;
+  file2.open("lname.txt"); //Open the inputted file name
+  char* out = new char[999];
+  int j = 0;
+  while(!file2.eof()) { //If has not reached end of file
+    file2.get(out[j]);
+    j++;
+  }
+  char* split2 = strtok(out, " ");
+  while (split2 != NULL) {
+    randLast.push_back(split2);
+    split2 = strtok(NULL, " ");
+  }
+
+
   int size = 100;
   Student** array = new Student*[size];
   bool start = true;
@@ -61,6 +94,12 @@ int main() {
     }
     else if (strcmp(input, "ADD RANDOM") == 0) {
       cout << "How many random students?" << endl;
+      char inNum[9];
+      cin.getline(inNum, 9);
+      int stuNum = atoi(inNum);
+      for (int q = 0; q < stuNum; q++) {
+        array = addStudent(array, randomStu(randFirst, randLast), size);
+      }
     }
     else if (strcmp(input, "HELP") == 0) {
       cout << "Type ADD to add a student\nType PRINT to print list\nType DELETE to delete student\nType QUIT to quit" << endl; 
@@ -86,9 +125,10 @@ Student** rehash(Student** array, int &size) {
   cout << "Rehashing" << endl;
   size = size * 2;
   Student** newArray = new Student*[size];
-  for (int b = 0; b <= size; b++) {
+  for (int b = 0; b <= size/2; b++) {
     if (array[b] != NULL) {
-      newArray = reAdd(newArray, array[b], size);
+      cout << "Found at: " << b << endl;
+      newArray = addStudent(newArray, array[b], size);
     }
   }
   return newArray;
@@ -136,37 +176,25 @@ int hashFunction(Student* stu, int size) {
   }
   hashNum = hashNum + round(stu->gpa);
   hashNum = hashNum + stu->id;
+  cout << "Gross index: " << hashNum << endl;
   int retNum = hashNum % size;
   return retNum;
 }
 
-Student** reAdd(Student** &array, Student* student, int &size) {
-  int index = hashFunction(student, size);
-  cout << index << endl;
-  if (array[index] == NULL) {
-    cout << "New" << endl;
-    array[index] = student;
-    return array;
-  }
-  else {
-    cout << checkCollision(array[index], 1) << endl;
-    if (checkCollision(array[index], 1) == 3) {
-      return rehash(array, size);
-    }
-    else {
-      endList(array[index])-> next = student;
-      return array;
-    }
-  }
-}
+
 
 Student** addStudent(Student** &array, Student* ptrStu, int &size) {
   int index = hashFunction(ptrStu, size);
   cout <<"Index: " << index << endl;
   if (array[index] == NULL) {
-    cout << "New" << endl;
+    cout << "New spot" << endl;
     array[index] = ptrStu;
-    return array;
+    if (ptrStu -> next != NULL) {
+      return (addStudent(array, ptrStu -> next, size));
+    }
+    else {
+      return array;
+    }
   }
   else {
     cout << "Collisions: " << checkCollision(array[index], 0) << endl;
@@ -196,21 +224,36 @@ void print(Student** &array, int size) {
 }
 
 
-Student* randomStu() {/*
-  ifstream file;
-  file.open(fname.txt); //Open the inputted file name
-  char* output = new char[999];
-  char* out = new char[999];
-  int i = 0;
-  while(!file.eof()) { //If has not reached end of file
-    file.get(output[i]);
-    i++;
+Student* randomStu(vector<char*> first, vector<char*> last) {
+  Student* newStu = new Student();
+  int fSize = first.size();
+  int lSize = last.size();
+  int randF = rand() % fSize;
+  int randL = rand() % lSize;
+  cout << randF << randL << endl;
+  int fIndex = 0;
+  int lIndex = 0;
+  vector<char*> :: iterator i;
+  for ( i = first.begin(); i != first.end(); i++) {
+    if (fIndex == randF) {
+      cout << (*i) << endl;
+      strcpy(newStu -> fname, (*i));
+    }
+    fIndex++;
   }
-  int a = 0;
-  if (output[a].isalpha() == true) {
-    
+  vector<char*> :: iterator it;
+  for ( it = last.begin(); it != last.end(); it++) {
+    if (lIndex == randL) {
+      cout << (*it) << endl;
+      strcpy(newStu -> lname, (*it));
+    }
+    lIndex++;
   }
-  else {
-    
-  }*/
+  int newID = rand() % 3000 + 2000;
+  newStu -> id = newID;
+  float newGPA = ((float) rand()) / ((float) RAND_MAX) + 3.0;
+  newGPA = (int) (newGPA * 10 + 0.5);
+  newGPA = (float) newGPA / 10;
+  newStu -> gpa = newGPA;
+  return newStu;
 }
